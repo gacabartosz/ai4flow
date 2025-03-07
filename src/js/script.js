bb33753bde5b334ab42ac393e308cad149c32ccf
gacabartosz <gaca.bartosz@gmail.com>
2025-03-07 22:57:11 +0100

    Final update: Particles.js fixed, design improved

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+console.log("Particles.js script loaded.");
+document.addEventListener("DOMContentLoaded", function () {
+    if (window.particlesJS) {
+        console.log("Particles.js is available.");
+    } else {
+        console.error("Particles.js is not loaded!");
+    }
+});
