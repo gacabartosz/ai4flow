8616fb00d0fe717a4852cdfaf169f24cb2e1436e
gacabartosz <89811211+gacabartosz@users.noreply.github.com>
2025-03-10 19:04:42 +0100

    Update particles-config.js

--- src/js/particles-config.js
+++ src/js/particles-config.js
@@ -1,20 +1,13 @@
 particlesJS("particles-js", {
     particles: {
-        number: { value: 80, density: { enable: true, value_area: 800 } },
+        number: { value: 80 },
         color: { value: "#ffffff" },
         shape: { type: "circle" },
-        opacity: { value: 0.5, random: false },
-        size: { value: 3, random: true },
-        move: { enable: true, speed: 2, direction: "none", random: false }
-    },
-    interactivity: {
-        detect_on: "canvas",
-        events: {
-            onhover: { enable: true, mode: "repulse" },
-            onclick: { enable: true, mode: "push" }
-        },
-        modes: {
-            repulse: { distance: 100, duration: 0.4 }
+        opacity: { value: 0.5 },
+        size: { value: 3 },
+        move: {
+            enable: true,
+            speed: 2
         }
     }
 });
